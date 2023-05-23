90d109b00f51b38472104846539127945948d310
aqquilin14 <92664718+aqquilin14@users.noreply.github.com>
2023-05-23 10:09:09 +0530

    Update main.py

--- CCL/C1/main.py
+++ CCL/C1/main.py
@@ -5,3 +5,6 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('hello')
 
 app = webapp2.WSGIApplication([('/',MainPage)], debug=True)
+
+
+#python <path_to_sdk>/bin/devappserver.py <path_to_application_directory>
